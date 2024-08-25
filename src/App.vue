10cd7c8bd2cd4f1b20e0942a97e57f7668ee727d
Rafael Lopez Bobadilla <rafael.lopez.bob@gmail.com>
2024-08-25 15:41:19 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+</script>
+
+<template>
+  <nav>
+    <RouterLink to="/">Home</RouterLink>
+    <RouterLink to="/search">Search</RouterLink>
+    <RouterLink to="/shows/someid">Show</RouterLink>
+  </nav>
+  <RouterView />
+</template>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+body {
+  background-color: rgb(12, 34, 136);
+  color: white;
+  font-family: 'Inter';
+}
+a {
+  text-decoration: none;
+  color: inherit;
+}
+@font-face {
+  font-family: 'Inter';
+  src: url('./assets/fonts/Inter-VariableFont_slnt,wght.ttf');
+}
+</style>
