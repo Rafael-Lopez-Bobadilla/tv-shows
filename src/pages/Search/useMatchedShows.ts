6435ea3e983d89cde261defcd923ed654790604d
Rafael Lopez Bobadilla <rafael.lopez.bob@gmail.com>
2024-09-10 01:43:56 +0200

    modified debouncer

--- src/pages/Search/useMatchedShows.ts
+++ src/pages/Search/useMatchedShows.ts
@@ -2,21 +2,29 @@ import showsStore from '@/stores/showsStore'
 import type { TShows } from '@/services/schemas/showsSchemas'
 import { ref, watchEffect } from 'vue'
 import { getShowsByQuery } from '@/services/services'
+
+function debounce(callback: (...args: any) => void, wait: number) {
+  let timerId: NodeJS.Timeout
+  return (...args: any) => {
+    clearTimeout(timerId)
+    timerId = setTimeout(() => {
+      callback(...args)
+    }, wait)
+  }
+}
 const useMatchedShows = () => {
   const text = ref('')
   const matchedShows = ref<TShows | undefined>(undefined)
   if (!showsStore.shows) showsStore.fetchData()
-  let timeout: NodeJS.Timeout | null = null
+  const myFunc = debounce(async () => {
+    const matched = await getShowsByQuery(text.value)
+    const shows = matched.map((matched) => matched.show)
+    if (matched) matchedShows.value = shows
+  }, 300)
   watchEffect(() => {
-    if (timeout) clearTimeout(timeout)
     if (text.value === '') matchedShows.value = showsStore.shows?.slice(0, 20)
     if (text.value !== '') {
-      timeout = setTimeout(async () => {
-        timeout = null
-        const matched = await getShowsByQuery(text.value)
-        const shows = matched.map((matched) => matched.show)
-        if (matched) matchedShows.value = shows
-      }, 300)
+      myFunc()
     }
   })
   return { text, matchedShows }
